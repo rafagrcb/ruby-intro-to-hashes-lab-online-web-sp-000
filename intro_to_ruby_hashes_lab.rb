0a011f83df67e04a2291e71afb7dfe10ea808ad3
Rafael Garcia <rafagrcb@gmail.com>
2020-06-15 14:41:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,13 +7,21 @@ actor = {name: "Dwayne The Rock Jhonson"}
 end
 
 def monopoly
-	monopoly = {
+	  monopoly = {
 	  :railroads =>{}
 	}
 
 end
 
 def monopoly_with_second_tier
+    monopoly = {
+		:railroads=>
+		{
+		:pieces=>4,
+		:names=>{},
+		:rent_in_dollars=>{}
+		}
+	}
 
 end
 
